72b3d76d5c284217222f94a74dc4e23538e70135
Mateus Logrado <mateus_logrado@estudante.sesisenai.org.br>
2025-07-08 14:30:28 -0300

    listar aluno

---
+++ js/aluno/listar.js
@@ -0,0 +1,37 @@
+let res = document.getElementById("res")
+let button = document.getElementById("button")
+
+button.addEventListener("click", () => {
+
+    res.innerHTML = `<table>
+    <tr>
+      <th>Nome</th>
+      <th>Sobrenome</th>
+      <th>Matricula</th>
+      <th>Telefone</th>
+      <th>Email</th>
+    </tr>`
+
+    res.innerHTML = ""
+
+    fetch("http://localhost:8081/aluno", {
+        method: "GET",
+        headers: {
+            "Content-Type": "Application/JSON"
+        }
+    })
+        .then(resp => resp.json())
+        .then(valores => {
+
+            valores.forEach(val => {
+                res.innerHTML += `  <tr>
+    <td>${val.nome}</td>
+    <td>${val.sobrenome}</td>
+    <td>${val.matricula}</td>
+    <td>${val.telefone}</td>
+    <td>${val.email}</td>
+  </tr>`
+            });
+        })
+    res.innerHTML += `</table>`
+})
