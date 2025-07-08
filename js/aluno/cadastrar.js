06eea4caa5d6ea6621c4f5aa2414cdc1cc8909c2
Mateus Logrado <mateus_logrado@estudante.sesisenai.org.br>
2025-07-08 14:21:39 -0300

    cadastrar no aluno

---
+++ js/aluno/cadastrar.js
@@ -0,0 +1,47 @@
+let res = document.getElementById("res")
+let button = document.getElementById("button")
+
+button.addEventListener("click", () => {
+    let nome = document.getElementById("nome").value
+    let sobrenome = document.getElementById("sobrenome").value
+    let matricula = document.getElementById("matricula").value
+    let telefone = document.getElementById("telefone").value
+    let email = document.getElementById("email").value
+
+    const valores = {
+        nome: nome,
+        sobrenome: sobrenome,
+        matricula: matricula,
+        telefone: telefone,
+        email: email
+    }
+
+    res.innerHTML = ""
+
+    fetch("http://localhost:8081/aluno", {
+        method: "POST",
+        headers: {
+            "Content-Type": "Application/JSON"
+        },
+        body: JSON.stringify(valores)
+    })
+        .then(resp => resp.body)
+        .then(() => {
+            res.innerHTML = `<table>
+  <tr>
+    <th>Nome</th>
+    <th>Sobrenome</th>
+    <th>Matricula</th>
+    <th>Telefone</th>
+    <th>Email</th>
+  </tr>
+  <tr>
+    <td>${nome}</td>
+    <td>${sobrenome}</td>
+    <td>${matricula}</td>
+    <td>${telefone}</td>
+    <td>${email}</td>
+  </tr>
+</table>`
+        })
+})
