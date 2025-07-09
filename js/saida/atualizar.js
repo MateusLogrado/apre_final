a6252d9ad4e9edbc412f2358c62b4cf486e6b198
Mateus Logrado <mateus_logrado@estudante.sesisenai.org.br>
2025-07-09 16:01:53 -0300

    atualizar Saida

---
+++ js/saida/atualizar.js
@@ -0,0 +1,113 @@
+let res = document.getElementById("res")
+let button = document.getElementById("button")
+let buscar = document.getElementById("buscar")
+
+button.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    let codSaida = Number(document.getElementById("codSaida").value)
+    
+
+    let dataSolicitacao = document.getElementById("dataSolicitacao").value
+    const dataAgora = String(dataSolicitacao)
+    const data = dataAgora.slice(0,10)
+    console.log(data)
+
+    let horaSaida = document.getElementById("horaSaida").value
+    let horaRetorno = document.getElementById("horaRetorno").value
+    let motivo = document.getElementById("motivo").value
+    let localDestino = document.getElementById("localDestino").value
+    let status = document.getElementById("status").value
+    let nomeAluno = document.getElementById("nomeAluno").value
+    let aluno_cod = document.getElementById("aluno_id").value
+    let nomeProfessor = document.getElementById("nomeProfessor").value
+    let professor_cod = document.getElementById("professor_id").value
+
+    const valores = {
+        dataSolicitacao: data,
+        horaSaida: horaSaida,
+        horaRetorno: horaRetorno,
+        motivo: motivo,
+        localDestino: localDestino,
+        status: status,
+        nomeAluno: nomeAluno,
+        aluno_cod: aluno_cod,
+        nomeProfessor: nomeProfessor,
+        professor_cod: professor_cod
+    }
+
+    res.innerHTML = ""
+
+    fetch(`http://localhost:8081/saida/${codSaida}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "Application/JSON"
+        },
+        body: JSON.stringify(valores)
+    })
+        .then(resp => resp.body)
+        .then(() => {
+            res.innerHTML = `<table>
+  <tr>
+            <th>Codigo</th>
+            <th>Data da Solicitação</th>
+            <th>Hora de Saída</th>
+            <th>Hora de Retorno</th>
+            <th>Motivo</th>
+            <th>Destino</th>
+            <th>Status</th>
+            <th>Aluno</th>
+            <th>Professor</th>
+  </tr>
+  <tr>
+            <td>${codSaida}</td>
+            <td>${data}</td>
+            <td>${horaSaida}</td>
+            <td>${horaRetorno}</td>
+            <td>${motivo}</td>
+            <td>${localDestino}</td>
+            <td>${status}</td>
+            <td>${nomeAluno}</td>
+            <td>${nomeProfessor}</td>
+  </tr>
+</table>`
+        })
+})
+
+buscar.addEventListener("click", (e)=>{
+    e.preventDefault()
+
+    let codSaida = Number(document.getElementById("codSaida").value)
+
+    let dataSolicitacao = document.getElementById("dataSolicitacao")
+    let horaSaida = document.getElementById("horaSaida")
+    let horaRetorno = document.getElementById("horaRetorno")
+    let motivo = document.getElementById("motivo")
+    let localDestino = document.getElementById("localDestino")
+    let status = document.getElementById("status")
+    let nomeAluno = document.getElementById("nomeAluno")
+    let nomeProfessor = document.getElementById("nomeProfessor")
+
+    res.innerHTML = ""
+
+    fetch(`http://localhost:8081/saida/${codSaida}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "Application/JSON"
+        }
+    })
+        .then(resp => resp.json())
+        .then(val => {
+            dataSolicitacao.value = val.dataSolicitacao
+            horaRetorno.value = val.horaRetorno
+            horaSaida.value = val.horaSaida
+            motivo.value = val.motivo
+            localDestino.value = val.localDestino
+            status.value = val.status
+            nomeAluno.value = val.nomeAluno
+            nomeProfessor.value = val.nomeProfessor
+        })
+    .catch((err)=>{
+        console.error("Erro: ", err)
+    })
+})
