06c97e7f9c20593e0020aead795a2080b9f90783
Mateus Logrado <mateus_logrado@estudante.sesisenai.org.br>
2025-07-10 17:06:41 -0300

    listar pedentes

--- js/saida/listar.js
+++ js/saida/listar.js
@@ -3,48 +3,104 @@ let button = document.getElementById("button")
 
 button.addEventListener("click", (e) => {
     e.preventDefault()
+    let listaOnline = document.getElementById("listaOnline").value
 
+    switch (listaOnline) {
+        case "todos":
+            todos()
+            break;
+        case "pendentes":
+            pendente()
+            console.log("chamando pedentes()")
+            break;
+    } 
+})
 
-
+function todos(){
     fetch("http://localhost:8081/saida", {
         method: "GET",
         headers: {
-            "Content-Type": "Application/JSON"
+            "Content-Type": "application/json"
         }
     })
-        .then(resp => resp.json())
-        .then(valores => {
-            let html = `<table>
-          <tr>
-            <th>Codigo</th>
-            <th>Data da Solicitação</th>
-            <th>Hora de Saída</th>
-            <th>Hora de Retorno</th>
-            <th>Motivo</th>
-            <th>Destino</th>
-            <th>Status</th>
-            <th>Aluno</th>
-            <th>Professor</th>
-          </tr>`
+    .then(resp => resp.json())
+    .then(valores => {
+        let html = `<table>
+            <tr>
+                <th>Código</th>
+                <th>Data da Solicitação</th>
+                <th>Hora de Saída</th>
+                <th>Hora de Retorno</th>
+                <th>Motivo</th>
+                <th>Destino</th>
+                <th>Status</th>
+                <th>Aluno</th>
+                <th>Professor</th>
+            </tr>`
         
-            valores.forEach(val => {
-                html += `          <tr>
-            <td>${val.codSaida}</td>               
-            <td>${val.dataSolicitacao}</td>
-            <td>${val.horaSaida}</td>
-            <td>${val.horaRetorno}</td>
-            <td>${val.motivo}</td>
-            <td>${val.localDestino}</td>
-            <td>${val.status}</td>
-            <td>${val.nomeAluno}</td>
-            <td>${val.nomeProfessor}</td>
-          </tr>`
-            });
-        
-            html += `</table>`
-            res.innerHTML = html
-        })
-    .catch((err)=>{
-        console.error("Erro: ", err)
+        valores.forEach(val => {
+            html += `<tr>
+                <td>${val.codSaida}</td>               
+                <td>${val.dataSolicitacao}</td>
+                <td>${val.horaSaida}</td>
+                <td>${val.horaRetorno}</td>
+                <td>${val.motivo}</td>
+                <td>${val.localDestino}</td>
+                <td>${val.status}</td>
+                <td>${val.nomeAluno}</td>
+                <td>${val.nomeProfessor}</td>
+            </tr>`
+        });
+
+        html += `</table>`
+        res.innerHTML = html
     })
-})
+    .catch(err => {
+        console.error("Erro:", err)
+    })
+}
+
+function pendente(){
+    fetch("http://localhost:8081/saida", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(resp => resp.json())
+    .then(valores => {
+        let html = `<table>
+            <tr>
+                <th>Código</th>
+                <th>Data da Solicitação</th>
+                <th>Hora de Saída</th>
+                <th>Hora de Retorno</th>
+                <th>Motivo</th>
+                <th>Destino</th>
+                <th>Status</th>
+                <th>Aluno</th>
+                <th>Professor</th>
+            </tr>`
+        valores.forEach(val => {
+            if (val.status === "pendente") {
+                html += `<tr>
+                    <td>${val.codSaida}</td>               
+                    <td>${val.dataSolicitacao}</td>
+                    <td>${val.horaSaida}</td>
+                    <td>${val.horaRetorno}</td>
+                    <td>${val.motivo}</td>
+                    <td>${val.localDestino}</td>
+                    <td>${val.status}</td>
+                    <td>${val.nomeAluno}</td>
+                    <td>${val.nomeProfessor}</td>
+                </tr>`
+            }
+        });
+
+        html += `</table>`
+        res.innerHTML = html
+    })
+    .catch(err => {
+        console.error("Erro:", err)
+    })
+}
